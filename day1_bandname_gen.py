4c5ecb9105627db2d9e1204c3b91f649bf8686ab
Hutchson <51213189+Gmstflex@users.noreply.github.com>
2023-09-02 10:54:22 +0300

    Add files via upload

---
+++ day1_bandname_gen.py
@@ -0,0 +1,8 @@
+#Greeting
+print("Welcome to the Band Name Generator.")
+#Enquire city name
+city = input("What's the name of the city you grew up in?\n")
+#Enquire pet name
+pet_name = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city.title()} {pet_name.title()}")
